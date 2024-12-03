2cb7f21f12b5db0c7c2733e704425077bed66212
Yazan26 <yazandawud75@gmail.com>
2024-12-03 21:46:18 +0100

    a
    a

---
+++ program2.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Threading.Tasks;
+using HiveMQtt.Client;
+using SimpleMqtt;
+using HiveMQtt.Client.Events;
+using Avans.StatisticalRobot;
+
+class Program2
+{
+    static async Task Main(string[] args)
+    {
+        var mqttClient = SimpleMqttClient.CreateSimpleMqttClientForHiveMQ("2aa2fd8d");
+
+        mqttClient.OnMessageReceived += OnMessageReceived;
+
+        await mqttClient.SubscribeToTopic("robot/control");
+
+        // Keep the application running
+        Console.ReadLine();
+    }
+
+    private static void OnMessageReceived(object? sender, SimpleMqttMessage e)
+    {
+        Console.WriteLine($"Received message: {e.Message} on topic: {e.Topic}");
+
+        // Handle control commands
+        if (e.Message == "forward")
+        {
+            Robot.Motors(200, 200);
+        }
+        else if (e.Message == "backward")
+        {
+            Robot.Motors(-200, -200);
+        }
+        else if (e.Message == "left")
+        {
+            Robot.Motors(-200, 200);
+        }
+        else if (e.Message == "right")
+        {
+            Robot.Motors(200, -200);
+        }
+        else if (e.Message == "stop")
+        {
+            Robot.Motors(0, 0);
+        }
+    }
+}
